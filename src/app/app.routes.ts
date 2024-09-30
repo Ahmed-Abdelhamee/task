b2c1d4c44e9aa77fa5e6bd174b7400386a9cf3b8
ahmed-Abdelhameed <medoabdelhameed3@gmail.com>
2024-09-30 03:23:21 +0300

    creating only required things

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { BookDetailsComponent } from './features/book-details/book-details.compo
 import { AuthorDetailsComponent } from './features/author-details/author-details.component';
 import { SearchComponent } from './features/search/search.component';
 import { WishlistComponent } from './shared/components/wishlist/wishlist.component';
+import { authGuard } from './core/gaurds/auth.guard';
 
 export const routes: Routes = [
-    {path:"",redirectTo:"home",pathMatch:"full"},
-    {path:"home",component:HomeComponent},
-    {path:"book-details/:id",component:BookDetailsComponent},
-    {path:"author-details/:id",component:AuthorDetailsComponent},
+    {path:"home",redirectTo:"",pathMatch:"full"},
+    {path:"",component:HomeComponent},
+    {path:"book-details/:id",component:BookDetailsComponent,canActivate:[authGuard]},
+    {path:"author-details/:id",component:AuthorDetailsComponent,canActivate:[authGuard]},
     {path:"search-item/:id",component:SearchComponent},
     {path:"wishlist",component:WishlistComponent},
 ];
